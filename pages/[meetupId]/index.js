d3f378e47f8826a617ded50731d6e6e3f17e26d0
phsu40 <phsu40@gatech.edu>
2023-05-21 08:35:39 -0700

    Create Meetup Detail component

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 const MeetupDetailsPage = () => {
   return (
-    <Fragment>
-      <h1>MeetupDetailsPage</h1>
-    </Fragment>
+    <MeetupDetail
+      image="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="This is a first meetup"
+    />
   );
 };
 
