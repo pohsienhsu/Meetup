4a65ea7348adce64cde3f0644a9579f299b94949
phsu40 <phsu40@gatech.edu>
2023-05-21 10:57:37 -0700

    Integrate MongoDB to store new meetup submissions

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,25 @@
 // /api/new-meetup
 // POST /api/new-meetup
 
-const handler = (req, res) => {
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const handler = async (req, res) => {
   if (req.method === "POST") {
     const data = req.body;
-    const { title, image, address, description } = data;
+
+    const mongodbURI =
+      "mongodb+srv://habohsu930mongodb:GSTsokgyWawejPBc@meetup-cluster-0.dy9ziyv.mongodb.net/meetups?retryWrites=true&w=majority";
+    const mongoClient = await MongoClient.connect(mongodbURI);
+    const db = mongoClient.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    mongoClient.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 };
 
